f65b15a2c35d9275c9ccf7ecab2250d798c60895
manaspalaparthi <manas.palaparthi@gmail.com>
2023-09-05 11:00:41 +1000

    wifi details

---
+++ wifi_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set your WiFi SSID and password
+SSID="cyberlab24"
+PASSWORD="IoTNetwork1234"
+
+# Edit the wpa_supplicant.conf file to configure WiFi for Australia (AU)
+cat <<EOL > /etc/wpa_supplicant/wpa_supplicant.conf
+country=AU
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+network={
+    ssid="$SSID"
+    psk="$PASSWORD"
+}
+EOL
+
+# Restart the networking service to apply changes
+sudo systemctl restart networking
